820abc61276d2cdb3e5e70234fe06c4045693198
Perchun Pak <git@perchun.it>
2023-06-07 08:35:12 +0000

    07.06.2023

--- scripts/integration-tests-gen/generate.py
+++ scripts/integration-tests-gen/generate.py
@@ -29,6 +29,7 @@ TODO
   - [ ] Replace strings.
   - [ ] Write `data.json` file for autogenerating.
 """
+from __future__ import annotations
 
 import configparser
 import hashlib
@@ -38,6 +39,7 @@ from collections import defaultdict
 from glob import glob
 from pathlib import Path
 import typing as t
+import dataclasses
 
 if t.TYPE_CHECKING:
     import typing_extensions as te
@@ -57,34 +59,65 @@ DATA_FOR_GENERATING = TEMPLATE_DIR / "data.json"
 TEMPLATE_FILE = TEMPLATE_DIR / "ci-yaml.txt"
 TEMPLATE_FILE_SERVICES = TEMPLATE_DIR / "ci-yaml-services.txt"
 
-def parse_minecraft_core(data: MinecraftCoreData) -> str:
-    parsed = ""
-    if "versions" in data:
-        assert len(data["versions"]) > 0 and data["image"] == "itzg/minecraft-server"
-    else:
-        data["versions"] = [None]
-        
-    for version in data["versions"]:
-        parsed += f"{id}:\n"
-        parsed += f"    image: {data['image']}\n"
 
-        if version:
-            data.setdefault("env", {})
-            data["env"]["VERSION"] = version
-        
-        if data["image"] == "itzg/minecraft-server":
-            data.setdefault("env", {})
-            data["env"]["EULA"] = "TRUE"
+@dataclasses.dataclass
+class ServiceInfo:
+    id: str
+    image: str
+    env: dict[str, str] | None
+    options: list[str] | None
 
-        if "env" in data:
-            parsed += f"    env:\n"
-            for key, value in data["env"].items():
-                parsed += f"      {key}: {value}\n"
+    @classmethod
+    def from_dict(cls, data: MinecraftCoreData) -> list[te.Self]:
+        output: list[ServiceInfo] = []
+        if "versions" in data:
+            assert len(data["versions"]) > 0 and data["image"] == "itzg/minecraft-server"
+        else:
+            data["versions"] = [None]
+            
+        for version in data["versions"]:
+            id = data["id"] + (f"-{version}" if version else '')
 
-        parsed += f"    options: >-"
-        parsed += f"\n              ".join(data["args"])
-        parsed += "\n\n\n"
-    return parsed
+            if version:
+                data.setdefault("env", {})
+                data["env"]["VERSION"] = version
+            
+            if data["image"] == "itzg/minecraft-server":
+                data.setdefault("env", {})
+                data["env"]["EULA"] = "TRUE"
+
+            output.append(cls(
+                id=id,
+                image=data["image"],
+                env=data.get("env"),
+                options=data.get("options"),
+            ))
+            
+        return output
+
+    def attach_port(self, port: int) -> None:
+        if self.image == "itzg/minecraft-server":
+            self.env = {} if self.env is None else self.env
+            self.env["SERVER_PORT"] = str(port)        
+        else:
+            assert self.env is not None
+            for key, value in self.env.items():
+                self.env[key] = value.replace("{{ PORT }}", str(port))
+
+    def to_yaml(self) -> str:
+        output = f"{self.id}:\n"
+        output += f"    image: {self.image}\n"
+
+        if self.env:
+            output += f"    env:\n"
+            for key, value in self.env.items():
+                output += f"      {key}: {value}\n"
+
+        if self.options:
+            output += f"    options: >-\n"
+            output += f"\n              ".join(self.options) + "\n"
+
+        return output
 
 
 def write_yaml_file(services: str) -> None:
@@ -116,8 +149,13 @@ def main(fail_on_changes: bool) -> None:
         data: list[MinecraftCoreData] = json.load(data_file)
 
     output = ""
+    ports = []
     for minecraft_core in data:
-        output += SERVICE_TEMPLATE.format(**minecraft_core)
+        as_objects = ServiceInfo.from_dict(minecraft_core)
+        for service in as_objects:
+            ports.append(port := 25565 + len(ports))
+            service.attach_port(port)
+            output += service.to_yaml() + "\n"
 
     write_yaml_file(output)
 
